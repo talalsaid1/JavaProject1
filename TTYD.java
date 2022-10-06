d2180e4d8839bcbc9d894cc42152c16c8b93d46a
Derek <swh376@nyu.edu>
2022-10-06 01:06:18 -0400

    Added control structure to prompt user for menu

--- TTYD.java
+++ TTYD.java
@@ -1,14 +1,35 @@
+import java.util.*;
+
 public class TTYD {
 	public static void main(String[] args) {
-		//recipe.printArray();
+        createRecipe forCreation = new createRecipe();
 
-        String[] testString = {"helllllooooooooo", "i am testing", "this is another test"};
+        
 
-        //Testing
-        createRecipe testing = new createRecipe();
-        testing.recipeTitle(testString);
-        testing.recipeDescription(testString);
-        testing.ingrdList(testString);
-        testing.instructions(testString);
+        while (true) {
+            //Asking user to select
+            Scanner askMain = new Scanner(System.in);
+            System.out.println("\n \n");
+            System.out.println("Enter a number to select from the following Menu Options: \n");
+            System.out.println("*-------------------------------------------------------* \n");
+            System.out.println("    1. Create recipe \n");
+            System.out.println("    2. Explore Recipes \n");
+            System.out.println("    3. Exit \n");
+            System.out.println("*-------------------------------------------------------* \n");
+
+            //User input
+            String userAnswer = askMain.nextLine();
+            if (userAnswer.equals("3")) {
+                break;
+            } else if (userAnswer.equals("1")) {
+                System.out.println("\n");
+                forCreation.callAll();
+            } else if (userAnswer.equals("2")) {
+                //Fill in with another callAll once created
+            } else {
+                System.out.println("Invalid Command, Try Again :)");
+                continue;
+            }
+        }
 	}   
 }
